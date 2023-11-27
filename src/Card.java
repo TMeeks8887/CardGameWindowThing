2ddd6c1492df29a207ce63f0ba49d84b830d3e07
teddymeeks <teddy.meek@menloschool.org>
2023-11-27 09:50:47 -0800

    adding instances

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Card {
+public class Card {
+
+    private String suit;
+    private int value;
+    private String rank;
+
+    public Card()
+    {
+        suit = "Diamonds";
+        value = '1';
+//        rank = "A" "2" "3" "4" "5" "6" "7" "8" "9" "10" "J" "Q" "K";
+
+    }
 }
