7534bfff8b7242ece2debd0a343fb68f7584563d
teddymeeks <teddy.meek@menloschool.org>
2023-11-29 00:01:49 -0800

    created the cards and deck, started the hand

---
+++ src/Player.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Player {
+
+    private ArrayList<Card> cards = new ArrayList<>();
+
+//    public Hand ()
+//    {
+//        this.cards = new ArrayList<>();
+//        Card card1 = cards.get(52);
+//        Card card2 = cards.get(51);
+//    }
+
+}
