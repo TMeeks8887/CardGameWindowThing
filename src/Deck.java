59382aa6012debc1f6e1902baabe70a529a40658
teddymeeks <teddy.meek@menloschool.org>
2023-12-06 09:54:54 -0800

    litteraly did so much, like everything is working, have to actually do the checking winner and if busting

--- src/Deck.java
+++ src/Deck.java
@@ -20,6 +20,8 @@ public class Deck
         cardsLeft = cards.size();
     }
 
+    public Deck() { }
+
     public ArrayList<Card> getArrList()
     {
         return cards;
@@ -41,15 +43,13 @@ public class Deck
 
     public Card deal()
     {
-        if (cards.isEmpty())
+        if (isEmpty())
         {
             return null;
         }
         cardsLeft--;
-        return cards.get(cardsLeft + 1);
+        return cards.get(cardsLeft);
     }
-
-
 
     public void shuffle()
     {
@@ -66,55 +66,6 @@ public class Deck
     public ArrayList<Card> getCards()
     {
         return this.cards;
-    }
-    //basic deck constructor
-    public Deck ()
-    {
-        this.cards = new ArrayList<>();
-        String numRank;
-        //all the Diamonds
-        for (int i = 2; i < 11; i++)
-        {
-            numRank = Integer.toString(i);
-            this.cards.add(new Card(numRank, "Diamond", i));
-        }
-        this.cards.add(new Card("Jack","Diamond", 10));
-        this.cards.add(new Card("Queen", "Diamond", 10));
-        this.cards.add(new Card("King","Diamond", 10));
-        this.cards.add(new Card("Ace","Diamond", 11));
-
-        //all the Spades
-        for (int i = 2; i < 11; i++)
-        {
-            numRank = Integer.toString(i);
-            this.cards.add(new Card(numRank,"Spade", i));
-        }
-        this.cards.add(new Card("Jack","Spade", 10));
-        this.cards.add(new Card("Queen","Spade", 10));
-        this.cards.add(new Card("King","Spade", 10));
-        this.cards.add(new Card("Ace","Spade", 11));
-
-        //all the Clubs
-        for (int i = 2; i < 11; i++)
-        {
-            numRank = Integer.toString(i);
-            this.cards.add(new Card(numRank,"Club", i));
-        }
-        this.cards.add(new Card("Jack","Club", 10));
-        this.cards.add(new Card("Queen","Club", 10));
-        this.cards.add(new Card("King","Club", 10));
-        this.cards.add(new Card("Ace","Club", 11));
-
-        //all the Hearts
-        for (int i = 2; i < 11; i++)
-        {
-            numRank = Integer.toString(i);
-            this.cards.add(new Card(numRank,"Heart", i));
-        }
-        this.cards.add(new Card("Jack", "Heart", 10));
-        this.cards.add(new Card("Queen","Heart", 10));
-        this.cards.add(new Card("King","Heart", 10));
-        this.cards.add(new Card("Ace","Heart", 11));
     }
 
 }
--- src/Game.java
+++ src/Game.java
@@ -6,21 +6,25 @@ import java.util.ArrayList;
 public class Game {
 
     Scanner s = new Scanner(System.in);
-
-    private Deck deck;
     private ArrayList<Player> players;
+    private int numPlayers;
+    private Deck wholeDeck;
+    private String hit;
 
     public Game()
     {
-        printInstructions();
-        deck = new Deck();
         players = new ArrayList<Player>();
-        String Ranks[] = {"2", "3", "4"};
-        int Values[] = {2, 3, 4};
+        numPlayers = 0;
+        String Ranks[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        int Values[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
         String Suits[] = {"Spades", "Hearts", "Diamonds", "Clubs"};
-        Deck wholeDeck = new Deck(Ranks, Suits, Values);
+        wholeDeck = new Deck(Ranks, Suits, Values);
+    }
+
+    public void activePlayers()
+    {
         System.out.println("How many players?");
-        int numPlayers = s.nextInt();
+        numPlayers = s.nextInt();
         System.out.print("");
         String name = s.nextLine();
 
@@ -31,8 +35,11 @@ public class Game {
             //want to add the players to an arraylist to be called on later
             players.add(new Player(name));
         }
-        System.out.println("The players, their points, and their cards are:");
-        System.out.println(players);
+    }
+
+    public void makeDealer()
+    {
+        players.add(new Player("dealer"));
     }
 
     public void printInstructions()
@@ -44,16 +51,62 @@ public class Game {
         System.out.println("The person closest to 21 wins!");
     }
 
+    public void askIfHit(Player name)
+    {
+        System.out.println("Does " + name.getName() + " want to hit? (put in yes/no or y/n)");
+        hit = s.nextLine();
+        if (hit.equals("yes") || hit.equals("y"))
+        {
+            name.addCard(wholeDeck.deal());
+            System.out.println(name.getCards());
+            askIfHit(name);
+        }
+    }
+
+    public void startingHand(Player name)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            name.addCard(wholeDeck.deal());
+        }
+    }
+
+    public void printPlayers()
+    {
+        System.out.println("The players, their points, and their cards are:");
+        System.out.println(players);
+    }
+
+    public void checkIf21(Player name)
+    {
+        if (name.getPoints() > 21)
+        {
+            System.out.println("You busted!");
+        }
+    }
+
+    public  void playGame()
+    {
+        printInstructions();
+        makeDealer();
+        activePlayers();
+        wholeDeck.shuffle();
+        // goes through each player to add cards to
+        for (int i = 0; i < numPlayers + 1; i++)
+        {
+            startingHand(players.get(i));
+        }
+        printPlayers();
+        for (int i = 0; i < numPlayers + 1; i++)
+        {
+            askIfHit(players.get(i));
+        }
+        printPlayers();
+    }
+
     public static void main(String[] args)
     {
         Game game = new Game();
+        game.playGame();
     }
-
-//    public void startingHand(Player name)
-//    {
-//        for (int i = 0; i < 2; i++)
-//        {
-//            name.addCard(Deck.deal());
-//        }
-//    }
 }
